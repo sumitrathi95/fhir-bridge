1d86a3cb78a0dc4b513fcf844e914241ce4c77d0
Pablo Pazos <pablo.pazos@cabolabs.com>
2020-05-18 13:45:44 -0300

    fixed issue on enum with missing text value for coded text

--- src/main/java/org/ehrbase/fhirbridge/opt/shareddefinition/DerDiagnoseDefiningcode.java
+++ src/main/java/org/ehrbase/fhirbridge/opt/shareddefinition/DerDiagnoseDefiningcode.java
@@ -4,13 +4,13 @@ import java.lang.String;
 import org.ehrbase.client.classgenerator.EnumValueSet;
 
 public enum DerDiagnoseDefiningcode implements EnumValueSet {
-  B972("B97.2", "B97.2", "ICD-10-GM-2020", "B97.2"),
+  B972("Koronaviren als Ursache von Krankheiten, die in anderen Kapiteln klassifiziert sind", "B97.2", "ICD-10-GM-2020", "B97.2"),
 
-  U071("U07.1", "U07.1", "ICD-10-GM-2020", "U07.1"),
+  U071("COVID-19, Virus nachgewiesen", "U07.1", "ICD-10-GM-2020", "U07.1"),
 
-  U072("U07.2", "U07.2", "ICD-10-GM-2020", "U07.2"),
+  U072("COVID-19, Virus nicht nachgewiesen", "U07.2", "ICD-10-GM-2020", "U07.2"),
 
-  B342("B34.2", "B34.2", "ICD-10-GM-2020", "B34.2");
+  B342("Infektion durch Koronaviren nicht näher bezeichneter Lokalisation", "B34.2", "ICD-10-GM-2020", "B34.2");
 
   private String value;
 
