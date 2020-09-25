3b27bdd7b44009ea9cc35de6271f2b6fcab98213
Renaud Subiger <renaud@subiger.com>
2020-09-25 11:47:15 +0200

    Remove unnecessary configuration

--- src/main/java/org/ehrbase/fhirbridge/FhirBridgeApplication.java
+++ src/main/java/org/ehrbase/fhirbridge/FhirBridgeApplication.java
@@ -1,20 +1,11 @@
 package org.ehrbase.fhirbridge;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchRestClientAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-@EnableAutoConfiguration(exclude = ElasticsearchRestClientAutoConfiguration.class)
-public class FhirBridgeApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(FhirBridgeApplication.class);
-    }
+@SpringBootApplication(exclude = ElasticsearchRestClientAutoConfiguration.class)
+public class FhirBridgeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FhirBridgeApplication.class, args);
