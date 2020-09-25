4402f51b0b52e7d4b5f76b313ad5680b6d5771c0
Renaud Subiger <renaud@subiger.com>
2020-09-25 11:42:18 +0200

    Configura FHIR Bridge with Postgres

--- src/main/java/org/ehrbase/fhirbridge/FhirBridgeApplication.java
+++ src/main/java/org/ehrbase/fhirbridge/FhirBridgeApplication.java
@@ -1,11 +1,14 @@
 package org.ehrbase.fhirbridge;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchRestClientAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = ElasticsearchRestClientAutoConfiguration.class)
 public class FhirBridgeApplication extends SpringBootServletInitializer {
 
     @Override
