5c389bb08b3cb4224a55a56ba84d38ddb4a90f59
Birgit Dell13Zoll <birgit.saalfeld@plri.de>
2020-11-12 23:52:32 +0100

    works nearly, but problems with zusaetzlicheAngabenZumZielort

--- src/main/java/org/ehrbase/fhirbridge/mapping/FhirObservationHistoryOfTravelOpenehrHistoryOfTravel.java
+++ src/main/java/org/ehrbase/fhirbridge/mapping/FhirObservationHistoryOfTravelOpenehrHistoryOfTravel.java
@@ -6,10 +6,12 @@ import org.ehrbase.fhirbridge.opt.reiseaktivitatcomposition.ReiseaktivitatCompos
 import org.ehrbase.fhirbridge.opt.reiseaktivitatcomposition.definition.ReiseaktivitatBestimmtesReisezielCluster;
 import org.ehrbase.fhirbridge.opt.reiseaktivitatcomposition.definition.ReiseaktivitatObservation;
 import org.ehrbase.fhirbridge.opt.shareddefinition.*;
+import org.elasticsearch.cluster.routing.DelayedAllocationService;
 import org.hl7.fhir.r4.model.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.math.BigDecimal;
+import java.util.List;
 
 import com.nedap.archie.rm.datatypes.CodePhrase;
 import com.nedap.archie.rm.datavalues.DvCodedText;
@@ -25,6 +27,13 @@ public class FhirObservationHistoryOfTravelOpenehrHistoryOfTravel {
     private static final Logger logger = LoggerFactory.getLogger(FhirObservationHistoryOfTravelOpenehrHistoryOfTravel.class);
 
     private FhirObservationHistoryOfTravelOpenehrHistoryOfTravel() {}
+
+    private static String loinc_url = "http://loinc.org";
+    private static String loinc_DateTravelStarted = "82752-7";
+    private static String loinc_DateOfDepartureFromTravelDestination = "91560-3";
+    private static String loinc_CityOfTravel = "94653-3";
+    private static String loinc_StateOfTravel = "82754-3";
+    private static String loinc_CountryOfTravel = "94651-7";
 
     public static ReiseaktivitatComposition map(Observation fhirObservation) {
 
@@ -54,30 +63,53 @@ public class FhirObservationHistoryOfTravelOpenehrHistoryOfTravel {
 
             // specific travel destination
 
-
             // list of destinations
             List<ReiseaktivitatBestimmtesReisezielCluster> destination = null;
 
-            // first destination
-            {
-                // create destination cluster
-                ReiseaktivitatBestimmtesReisezielCluster cluster = new ReiseaktivitatBestimmtesReisezielCluster();
+            ReiseaktivitatBestimmtesReisezielCluster cluster = new ReiseaktivitatBestimmtesReisezielCluster();
 
-                //  set land
-                String code = fhirObservation.getComponent().get(1).getValueCodeableConcept().getCoding().get(0).getCode();
-                cluster.setLandDefiningcode(LandDefiningcode.createByCode(code));
+            for (int i = 0; i < fhirObservation.getComponent().size(); i++) {
 
-                // set bundesland
-                code = fhirObservation.getComponent().get(2).getValueCodeableConcept().getCoding().get(0).getCode();
-                cluster.setBundeslandRegionDefiningcode(BundeslandRegionDefiningcode.createByCode(code));
+                String urlOfCodingSystem;
+                String codeOfConcept;
 
-                // set city
-                cluster.setStadtValue(fhirObservation.getComponent().get(3).getValue().toString());
+                urlOfCodingSystem = fhirObservation.getComponent().get(i).getCode().getCoding().get(0).getSystem();
+                codeOfConcept = fhirObservation.getComponent().get(i).getCode().getCoding().get(0).getCode();
 
-                // store cluster in the list
-                destination.add(cluster);
-
+                // check for loinc code
+                if (!urlOfCodingSystem.equals(loinc_url)) {
+                    throw new UnprocessableEntityException("Expected loinc-code, but got '" + urlOfCodingSystem + "' instead ");
+                }
+                java.time.ZonedDateTime dateTime = null;
+                String contentString = null;
+                //do the mapping concrete
+                if (codeOfConcept.equals(loinc_DateTravelStarted)){
+                    dateTime = fhirObservation.getComponent().get(i).getValueDateTimeType().getValueAsCalendar().toZonedDateTime();
+                    observation.setAbreisedatumValue(dateTime);
+                }
+                else if (codeOfConcept.equals(loinc_DateOfDepartureFromTravelDestination)){
+                   dateTime = fhirObservation.getComponent().get(i).getValueDateTimeType().getValueAsCalendar().toZonedDateTime();
+                   observation.setRuckreisedatumValue(dateTime);
+                }
+                else if (codeOfConcept.equals(loinc_CityOfTravel)) {
+                    contentString = fhirObservation.getComponent().get(i).getValue().toString();
+                        cluster.setStadtValue(contentString);
+                }
+                else if (codeOfConcept.equals(loinc_StateOfTravel)) {
+                    contentString = fhirObservation.getComponent().get(i).getValueCodeableConcept().getCoding().get(0).getCode();
+                    cluster.setBundeslandRegionDefiningcode(BundeslandRegionDefiningcode.createByCode(contentString));
+                }
+                else if (codeOfConcept.equals(loinc_CountryOfTravel)) {
+                    contentString = fhirObservation.getComponent().get(i).getValueCodeableConcept().getCoding().get(0).getCode();
+                    cluster.setLandDefiningcode(LandDefiningcode.createByCode(contentString));
+                }
+                else {
+                    throw new UnprocessableEntityException("Expected loinc-code for history of travel, but got '" + codeOfConcept + "' instead ");
+                }
             }
+
+            // store cluster in the list
+            destination.add(cluster);
 
             // Destination of travel
             observation.setBestimmtesReiseziel(destination); // Why?
@@ -88,13 +120,21 @@ public class FhirObservationHistoryOfTravelOpenehrHistoryOfTravel {
         }
 
         composition.setReiseaktivitat(observation);
+        // admin entry : travel history
+        // each node needs a value
+
+        //template fields should be optional
+        //null for template for codes without value
+        // ask Julian for mandatory value
+
+        // null favour openEHR in template aktivieren
 
         // Required fields by API
         composition.setLanguage(Language.DE); // FIXME: we need to grab the language from the template
         composition.setLocation("test");
         composition.setSettingDefiningcode(SettingDefiningcode.SECONDARY_MEDICAL_CARE);
         composition.setTerritory(Territory.DE);
-        composition.setCategoryDefiningcode(CategoryDefiningcode.EVENT);
+        composition.setCategoryDefiningcode(CategoryDefiningcode.EVENT); //required by model
         composition.setStartTimeValue(fhirEffectiveDateTime.getValueAsCalendar().toZonedDateTime());
         composition.setComposer(new PartySelf());
 
